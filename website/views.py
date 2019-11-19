3758b4f1479fb0682b5ec630b0c1db99b5390fbe
devdantas <mauriciodantas.contato01@bol.com.br>
2019-11-18 21:54:56 -0300

    django-template

---
+++ website/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(req):
+    nome = 'Groger'
+    idade = 17
+    lista_roupas = [
+        'Boné da Lacoste',
+        'Boné da Cyclone',
+        'Cinto do Bátima',
+        'Toca da Medusa',
+        'Óculos Rift Zika'
+    ]
+
+    args = {
+        'roupas': lista_roupas,
+        'nome': nome,
+        'idade': idade
+    }
+    return render(req, 'home.html', args)
