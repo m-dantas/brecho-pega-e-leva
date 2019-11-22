9d10c3ed7f7ac1bc89550b47dd967c47643a305e
devdantas <mauriciodantas.contato01@bol.com.br>
2019-11-21 22:07:09 -0300

    criando models class

--- website/models.py
+++ website/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Produto(models.Model):
+    opcoes_tamanho = [
+        ('PP', 'Extra pequeno'),
+        ('P', 'Pequeno'),
+        ('M', 'Médio'),
+        ('G', 'Grande'),
+        ('GG', 'Extra grande')
+    ]
+
+    nome = models.CharField(max_length=50)
+    preco = models.DecimalField(decimal_places=2, max_digits=1000,  default=50)
+    disponivel = models.BooleanField(default=True)
+    quantidade = models.IntegerField(default=1)
+    tamanho = models.CharField(max_length=2, choices=opcoes_tamanho)
+
+class Pedido(models.Model):
+    metodo_pagamento = [
+        ('AV', 'Pagamento à vista'),
+        ('P2', 'Parcelado em 2x'),
+        ('P3', 'Parcelado em 3x')
+    ]
+
+    nome = models.CharField(max_length=120)
+    email = models.EmailField()
+    cartao = models.IntegerField()
+    pagamento = models.CharField(max_length=2, choices=metodo_pagamento)
